effb3293dbecc04603d4d5fe8e69cbc095aa65e5
IBRA <ibrahimalebrahim17@gmail.com>
2026-06-29 15:56:25 +0300

    setting some auth stuff and the api routes

--- app/auth/[pathname]/page.tsx
+++ app/auth/[pathname]/page.tsx
@@ -1,10 +1,15 @@
 import {AuthView} from "@neondatabase/auth/react";
 
-export default async function  AuthPage(){
+export default async function AuthPage(
+    {
+        params
+    }:{params:Promise<{pathname:string}>
+    }) {
+    const {pathname} = await params;
     return (
-        <div>
-            <div>
-                <AuthView/>
+        <div className="relative">
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 translate-y-1/5 w-full max-w-md">
+                <AuthView pathname = {pathname}  />
             </div>
         </div>
     );
